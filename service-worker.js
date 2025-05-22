392778d1a679bd27af859f5aa4b7ec712d36d67a
richard141271 <68450987+richard141271@users.noreply.github.com>
2025-05-22 09:03:01 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('ai-innovate-cache').then(cache => {
+    caches.open('ai-innovate-cache.v.2').then(cache => {
       return cache.addAll([
         'index.html',
         'profil.html',
